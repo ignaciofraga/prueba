76deb62f1f32190a9bb9ae18c219c4c06f1e14ea
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-09-05 11:15:59 +0200

    Update 02_ENTRADA_DATOS.py

--- pages/02_ENTRADA_DATOS.py
+++ pages/02_ENTRADA_DATOS.py
@@ -188,7 +188,7 @@ for archivo_subido in listado_archivos_subidos:
     con_engine = 'postgresql://' + usuario + ':' + contrasena + '@' + direccion_host + ':' + str(puerto) + '/' + base_datos
     engine = create_engine(con_engine)
     st.text(con_engine)
-#    engine = create_engine('postgresql://postgres:m0nt34lt0@193.146.155.99:5432/COAC')
+    engine = create_engine('postgresql://postgres:m0nt34lt0@193.146.155.99:5432/COAC')
     datos_estaciones.to_sql('estaciones', engine, if_exists='replace')
     
     # conn = init_connection()           
