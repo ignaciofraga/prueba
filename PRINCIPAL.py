39b60f982eae1b22cf62af20ec9f8f46696040a3
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-09-20 15:41:22 +0200

    Update PRINCIPAL.py

--- PRINCIPAL.py
+++ PRINCIPAL.py
@@ -29,22 +29,41 @@ def principal():
     # Añade el logo del IEO
     st.image(imagen_pagina)
 
-ivar, username = AUXILIAR.check_password()
-st.text(username)
 
-if AUXILIAR.check_password():
-    #st.write("USUARIO IDENTIFICADO CORRECTAMENTE")
+def password_entered():
+    """Checks whether a password entered by the user is correct."""
+    if (
+        st.session_state["username"] in st.secrets["passwords"]
+        and st.session_state["password"]
+        == st.secrets["passwords"][st.session_state["username"]]
+    ):
+        st.session_state["password_correct"] = True
+        del st.session_state["password"]  # don't store username + password
+        del st.session_state["username"]
+    else:
+        st.session_state["password_correct"] = False
+
+
+
+st.text('Introduzca sus datos de usuario y contraseña para acceder al servicio')
+username = st.text_input("Usuario", on_change=password_entered, key="username")
+st.text_input(
+    "Contraseña", type="password", on_change=password_entered, key="password"
+)
+
+# if AUXILIAR.check_password():
+#     #st.write("USUARIO IDENTIFICADO CORRECTAMENTE")
     
     
-    paginas = {"PRINCIPAL": principal,
-        "ENTRADA DATOS": AUXILIAR.entrada_datos,
-        "CONSULTA ESTADO": AUXILIAR.consulta_estado}
-    #    "CONSULTA ESTADILLOS": TEMP.CONSULTA_ESTADILLOS
-    #}
+#     paginas = {"PRINCIPAL": principal,
+#         "ENTRADA DATOS": AUXILIAR.entrada_datos,
+#         "CONSULTA ESTADO": AUXILIAR.consulta_estado}
+#     #    "CONSULTA ESTADILLOS": TEMP.CONSULTA_ESTADILLOS
+#     #}
     
-    seleccion = st.sidebar.selectbox("Elige la página: ",tuple(paginas.keys()))
+#     seleccion = st.sidebar.selectbox("Elige la página: ",tuple(paginas.keys()))
     
-    paginas[seleccion]()
+#     paginas[seleccion]()
 
 # # Autentica al usuario
 # if FUNCIONES_AUXILIARES.check_password():
