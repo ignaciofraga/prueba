034c335864d543b0d91e076e15ce13bc36504de4
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-09-21 09:25:02 +0200

    Update FUNCIONES_AUXILIARES.py

--- FUNCIONES/FUNCIONES_AUXILIARES.py
+++ FUNCIONES/FUNCIONES_AUXILIARES.py
@@ -31,8 +31,8 @@ def check_password():
             == st.secrets["passwords"][st.session_state["username"]]
         ):
             st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store username + password
-            del st.session_state["username"]
+#            del st.session_state["password"]  # don't store username + password
+#            del st.session_state["username"]
         else:
             st.session_state["password_correct"] = False
 
