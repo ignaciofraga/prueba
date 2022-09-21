2a6e6e30db3c1de4c590796be4de1c2e943c5a09
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-09-21 10:26:29 +0200

    Update FUNCIONES_AUXILIARES.py

--- FUNCIONES/FUNCIONES_AUXILIARES.py
+++ FUNCIONES/FUNCIONES_AUXILIARES.py
@@ -29,22 +29,17 @@ def check_password():
     listado_contrasenas = df_usuarios['password']
     
     
-    """Returns `True` if the user had a correct password."""
+    # Devuelve "True" si la constraseña es correcta 
 
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        # if (
-        #     st.session_state["username"] in st.secrets["passwords"]
-        #     and st.session_state["password"]
-        #     == st.secrets["passwords"][st.session_state["username"]]
-        # ):
-        if (
-            st.session_state["username"] in listado_contrasenas
-            and st.session_state["password"]
-            == listado_contrasenas[st.session_state["username"]]
-        ):
-            
-            st.session_state["password_correct"] = True
+    def password_entered(listado_usuarios,listado_contrasenas,usuario,contrasena):
+        
+        io_autorizado = 0
+        for iusuario_bd in range(len(listado_usuarios)):
+            if usuario == listado_usuarios[iusuario_bd] and contrasena == listado_contrasenas[iusuario_bd]:
+                io_autorizado = 1
+                
+        if io_autorizado == 1:
+           st.session_state["password_correct"] = True
         else:
             st.session_state["password_correct"] = False
 
@@ -54,10 +49,10 @@ def check_password():
         st.write('Introduzca sus datos de usuario y contraseña para acceder al servicio')
         col1, col2 = st.columns(2,gap="small")
         with col1:
-            st.selectbox('Selecciona el usuario',(listado_usuarios),on_change=password_entered, key="username")
+            usuario = st.selectbox('Selecciona el usuario',(listado_usuarios),on_change=password_entered, key="username")
         #st.text_input("Usuario", on_change=password_entered, key="username")
         with col2:
-            st.text_input("Contraseña", type="password", on_change=password_entered, key="password")
+            contrasena = st.text_input("Contraseña", type="password", on_change=password_entered, key="password")
 
         return False
     elif not st.session_state["password_correct"]:
@@ -65,10 +60,10 @@ def check_password():
         st.write('Introduzca sus datos de usuario y contraseña para acceder al servicio')
         col1, col2 = st.columns(2,gap="small")
         with col1:
-            st.selectbox('Selecciona el usuario',(listado_usuarios),on_change=password_entered, key="username")
+            usuario = st.selectbox('Selecciona el usuario',(listado_usuarios),on_change=password_entered, key="username")
         #st.text_input("Usuario", on_change=password_entered, key="username")
         with col2:
-            st.text_input("Contraseña", type="password", on_change=password_entered, key="password")
+            contrasena = st.text_input("Contraseña", type="password", on_change=password_entered, key="password")
         st.error("Usuario no reconocido o contraseña incorrecta.")
         return False
     else:
