7d673e8ce47e250a7f313d88a14d3921c807da9d
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-09-21 09:24:12 +0200

    s

--- PRINCIPAL.py
+++ PRINCIPAL.py
@@ -25,31 +25,41 @@ st.set_page_config(page_title="IEO NUTRIENTES", layout="wide",page_icon=logo_IEO
 
 
 
-with st.form("Formulario autenticación"):
+# with st.form("Formulario autenticación"):
 
-    listado_usuarios = ['1','2']
+#     listado_usuarios = ['1','2']
  
-    col1, col2 = st.columns(2,gap="small")
-    with col1:
-        tipo_usuario_elegido = st.selectbox('Selecciona el tipo de usuario', (listado_usuarios))
-    with col2:
-        contrasena           = st.text_input("Contraseña", type="password")
+#     col1, col2 = st.columns(2,gap="small")
+#     with col1:
+#         tipo_usuario_elegido = st.selectbox('Selecciona el tipo de usuario', (listado_usuarios))
+#     with col2:
+#         contrasena           = st.text_input("Contraseña", type="password")
 
-    # Botón de envío para confirmar selección
-    submitted = st.form_submit_button("Submit")
+#     # Botón de envío para confirmar selección
+#     submitted = st.form_submit_button("Submit")
 
 
-# # Si el usuario está autorizado, despliega las webs a las que tiene acceso
-# if FUNCIONES_AUXILIARES.check_password() is True:
+
+
+
+
+
+
+
+
+
+# Si el usuario está autorizado, despliega las webs a las que tiene acceso
+if FUNCIONES_AUXILIARES.check_password() is True:
     
+    st.text(st.session_state["username"])
     
-#     paginas = {"PRINCIPAL": PAGINAS.principal,
-#                "ENTRADA DATOS": PAGINAS.entrada_datos,
-#                "CONSULTA ESTADO": PAGINAS.consulta_estado}
+    paginas = {"PRINCIPAL": PAGINAS.principal,
+                "ENTRADA DATOS": PAGINAS.entrada_datos,
+                "CONSULTA ESTADO": PAGINAS.consulta_estado}
     
-#     seleccion = st.sidebar.selectbox("Elige la página: ",tuple(paginas.keys()))
+    seleccion = st.sidebar.selectbox("Elige la página: ",tuple(paginas.keys()))
     
-#     paginas[seleccion]()
+    paginas[seleccion]()
 
 # # Identificación y acceso
 # io_acceso, usuario = FUNCIONES_AUXILIARES.check_password()
@@ -61,13 +71,13 @@ with st.form("Formulario autenticación"):
 #     if usuario == '1':
     
 #         paginas = {"PRINCIPAL": PAGINAS.principal,
-#                    "ENTRADA DATOS": PAGINAS.entrada_datos,
-#                    "CONSULTA ESTADO": PAGINAS.consulta_estado}
+#                     "ENTRADA DATOS": PAGINAS.entrada_datos,
+#                     "CONSULTA ESTADO": PAGINAS.consulta_estado}
  
 #     if usuario == '2':
     
 #         paginas = {"PRINCIPAL": PAGINAS.principal,
-#                    "ENTRADA DATOS": PAGINAS.entrada_datos}
+#                     "ENTRADA DATOS": PAGINAS.entrada_datos}
                
 #     seleccion = st.sidebar.selectbox("Elige la página: ",tuple(paginas.keys()))
     
