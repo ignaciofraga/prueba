4dfc13fa6a43bc07e5b09b6c6f09a11e5c522fdd
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-10-21 12:07:01 +0200

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -1596,7 +1596,7 @@ def entrada_estado_mar():
 
                    
             with col2:
-                velocidad_viento  = st.number_input('Vel.Viento(m/s):',value=float(velocidad_viento_defecto),min_value=0)
+                velocidad_viento  = st.number_input('Vel.Viento(m/s):',value=float(velocidad_viento_defecto),min_value=float(0))
                 direccion_viento  = st.selectbox('Dir.Viento:',(direcciones),index = indice_direccion_viento_defecto)
                 pres_atmosferica  = st.number_input('Presion atm.(mmHg):',format='%i',value=pres_atmosferica_defecto,min_value=0)
                 for idato_beaufort in range(len(beaufort_nombre)):
