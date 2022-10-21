dd87fb24a10ade518e98f23f3248bf330d991b36
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-10-21 12:06:02 +0200

    s

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -1596,7 +1596,7 @@ def entrada_estado_mar():
 
                    
             with col2:
-                velocidad_viento  = st.number_input('Vel.Viento(m/s):',value=velocidad_viento_defecto,min_value=0)
+                velocidad_viento  = st.number_input('Vel.Viento(m/s):',value=float(velocidad_viento_defecto),min_value=0)
                 direccion_viento  = st.selectbox('Dir.Viento:',(direcciones),index = indice_direccion_viento_defecto)
                 pres_atmosferica  = st.number_input('Presion atm.(mmHg):',format='%i',value=pres_atmosferica_defecto,min_value=0)
                 for idato_beaufort in range(len(beaufort_nombre)):
@@ -1605,7 +1605,7 @@ def entrada_estado_mar():
                 viento_beaufort  = st.selectbox('Viento Beaufort:',(beaufort_nombre),index=indice_prop)
                 
             with col3:
-                 altura_ola  = st.number_input('Altura de ola(m):',value=round(altura_ola_defecto),min_value=0)
+                 altura_ola  = st.number_input('Altura de ola(m):',value=float(altura_ola_defecto),min_value=0)
                  for idato_douglas in range(len(doglas_nombre)):
                      if altura_ola == 0:
                          indice_prop = 0
@@ -1617,7 +1617,7 @@ def entrada_estado_mar():
                  mar_direccion = st.selectbox('Dir.Oleaje:',(direcciones),index = indice_mar_direccion_defecto)
  
             with col4:
-                 temp_aire     = st.number_input('Temperatura del aire(ºC):',value=temp_aire_defecto,min_value=0)
+                 temp_aire     = st.number_input('Temperatura del aire(ºC):',value=float(temp_aire_defecto),min_value=0)
                  marea         = st.selectbox('Marea:',(mareas),index = indice_marea_defecto)
                  prof_secchi   = st.number_input('Prof.Sechi(m):',value=prof_secchi_defecto,min_value=0)
                  max_clorofila = st.number_input('Max.Clorofila(m):',value=max_clorofila_defecto,min_value=0)
