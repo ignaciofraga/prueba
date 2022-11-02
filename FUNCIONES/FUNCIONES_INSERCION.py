1076a01ab0563c767ebf9279e8757bae1ef90005
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-11-02 11:44:09 +0100

    2

--- FUNCIONES/FUNCIONES_INSERCION.py
+++ FUNCIONES/FUNCIONES_INSERCION.py
@@ -1180,7 +1180,7 @@ def lectura_btl(nombre_archivo,datos_archivo,nombre_programa,direccion_host,base
         datos_botellas['latitud']                  = lat_estacion  
         datos_botellas['longitud']                 = lon_estacion
         datos_botellas['fecha_muestreo']           = fecha_salida
-        datos_botellas,textos_aviso                = FUNCIONES_INSERCION.control_calidad(datos_botellas,direccion_host,base_datos,usuario,contrasena,puerto)            
+        datos_botellas,textos_aviso                = control_calidad(datos_botellas,direccion_host,base_datos,usuario,contrasena,puerto)            
         
         # Añade columnas con datos del muestreo 
         datos_botellas['id_estacion_temp']         = numpy.zeros(datos_botellas.shape[0],dtype=int)
