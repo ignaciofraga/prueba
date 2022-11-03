f6b78f38fd52f09d64e6c5b8fdede429ff9f504e
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-11-03 12:23:05 +0100

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -1931,7 +1931,7 @@ def control_calidad_botellas():
             with col2:
                 
                 enunciado          = 'QF del muestreo'
-                valor_asignado     = st.radio(enunciado,texto_indice,horizontal=True)
+                valor_asignado     = st.radio(enunciado,texto_indice,horizontal=True,key = idato)
                 qf_asignado[idato] = indice_validacion[texto_indice.index(valor_asignado)]
         
         st.form_submit_button("Asignar los índices seleccionados")  
