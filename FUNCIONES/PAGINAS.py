15468b599188ec9fc1396f1e71c8a276746522ed
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-11-07 12:37:42 +0100

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -1466,7 +1466,7 @@ def entrada_salidas_mar():
         # Añade una columna con el nombre del buque utilizado
         df_salidas_radiales['Buque'] = [None]*df_salidas_radiales.shape[0]
         for isalida in range(df_salidas_radiales.shape[0]):
-            df_salidas_radiales['Buque'][isalida] = df_buques['nombre_buque'][df_buques['id_buque']==df_salidas_radiales['buque'][isalida]]
+            df_salidas_radiales['Buque'][isalida] = df_buques['nombre_buque'][df_buques['id_buque']==df_salidas_radiales['buque'].iloc[isalida]]
         
         # Elimina las columnas que no interesa mostrar
         df_salidas_radiales = df_salidas_radiales.drop(columns=['id_salida','programa','nombre_programa','hora_salida','fecha_retorno','hora_retorno','buque'])
