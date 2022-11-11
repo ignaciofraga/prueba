2a60969f78e5036c69fcaa31a3dc558b7ba0869f
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-11-11 09:51:39 +0100

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -2480,8 +2480,10 @@ def procesado_nutrientes():
         anho_referencia = 2000 
         st.text(df_seleccion['fecha_muestreo'].iloc[0])
         df_seleccion    = df_seleccion.sort_values('fecha_muestreo')
-        fecha_minima    = (df_seleccion['fecha_muestreo'].iloc[0]).replace(year=anho_referencia) - datetime.timedelta(days=dias_offset)
-        fecha_maxima    = (df_seleccion['fecha_muestreo'].iloc[-1]).replace(year=anho_referencia) + datetime.timedelta(days=dias_offset)  
+        fecha_minima    = df_seleccion['fecha_muestreo'].iloc[0] - datetime.timedelta(days=dias_offset)
+        fecha_minima    = fecha_minima.replace(year=anho_referencia)
+        fecha_maxima    = df_seleccion['fecha_muestreo'].iloc[-1] + datetime.timedelta(days=dias_offset)  
+        fecha_maxima    = fecha_maxima.replace(year=anho_referencia)
         
         st.text(fecha_minima)
         st.text(fecha_maxima)
