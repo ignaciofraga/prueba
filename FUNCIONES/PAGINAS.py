461a4b98887a19cf9df2c0916ac5551871064865
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-11-15 13:07:18 +0100

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -2440,6 +2440,7 @@ def procesado_nutrientes():
             datos_muestras = datos_corregidos[datos_corregidos['muestreo'].isnull() == False]
      
             st.text(datos_corregidos)
+            st.text(datos_corregidos['muestreo'])
             st.text(datos_muestras)
         
             listado_salidas            = datos_muestras['id_salida'].unique()
