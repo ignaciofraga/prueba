ede3f1a9f7516abb52085399a88ba79947866cfe
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-11-17 09:02:51 +0100

    Update FUNCIONES_INSERCION.py

--- FUNCIONES/FUNCIONES_INSERCION.py
+++ FUNCIONES/FUNCIONES_INSERCION.py
@@ -1464,7 +1464,7 @@ def control_calidad_nutrientes(datos_procesados,listado_variables,direccion_host
 
         st.text(df_seleccion['nitrato'])
         st.text(df_seleccion['ph'])
-        if not df_seleccion['ph'].isnull().all():
+        if all(v is None for v in df_seleccion['ph']):
             st.text('sososl')
         if not df_seleccion['ph'].isnull().all():
             az.plot(df_disponible_bd['nitrato'],df_disponible_bd['ph'],'.',color='#C0C0C0')
