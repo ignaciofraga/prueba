07042380e999970d956b41fa82511a2ede8b55d0
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-11-23 12:44:32 +0100

    Update FUNCIONES_INSERCION.py

--- FUNCIONES/FUNCIONES_INSERCION.py
+++ FUNCIONES/FUNCIONES_INSERCION.py
@@ -1083,8 +1083,9 @@ def lectura_btl(nombre_archivo,datos_archivo,nombre_programa,direccion_host,base
     
     
     # Identifica la estación a la que corresponde el archivo
-    posicion_separador = nombre_archivo.index('+')
-    nombre_estacion    = nombre_archivo[9:posicion_separador].upper() #+ 'CO'                
+    posicion_inicio    = nombre_archivo.find('e') + 1
+    posicion_final     = nombre_archivo.find('.')
+    nombre_estacion    = nombre_archivo[posicion_inicio:posicion_final] #+ 'CO'                
     id_estacion        = df_estaciones_radiales['id_estacion'][df_estaciones_radiales['nombre_estacion']==nombre_estacion].iloc[0] 
     
     # Identifica la fecha del muestreo
