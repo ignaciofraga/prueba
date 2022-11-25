0e68f2e5fb67d3310d6a9eebbb40b3c2c31a8403
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-11-25 11:48:01 +0100

    Update FUNCIONES_INSERCION.py

--- FUNCIONES/FUNCIONES_INSERCION.py
+++ FUNCIONES/FUNCIONES_INSERCION.py
@@ -1521,9 +1521,9 @@ def control_calidad_biogeoquimica(datos_procesados,variables_procesado,variables
         num_intervalos = 5
         val_intervalo  =  (vmax_rango - vmin_rango)/num_intervalos
         ax.set_xlim([vmin_rango, vmax_rango])
-        ax.set_xticks(numpy.arange(vmin_rango,vmax_rango+val_intervalo,val_intervalo))
+        ax.set_xticks(numpy.arange(vmin_rango,vmax_rango+val_intervalo,val_intervalo),rotation=45)
         ax.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))   
-        ax.set_xticks(rotation = 45)
+        #ax.set_xticks(rotation = 45)
         
         # Añade la leyenda
         ax.legend(loc='upper center',bbox_to_anchor=(0.5, 1.15),ncol=2, fancybox=True,fontsize=7)
