d78c35d3efaa3dcdbad4d4cac4c1324988d3882f
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-11-28 08:51:34 +0100

    oo

--- FUNCIONES/FUNCIONES_INSERCION.py
+++ FUNCIONES/FUNCIONES_INSERCION.py
@@ -1386,6 +1386,10 @@ def control_calidad_biogeoquimica(datos_procesados,variables_procesado,variables
     df_indices_calidad        = psql.read_sql('SELECT * FROM indices_calidad', conn)
     conn.close()
 
+    # Reemplaza los nan por None
+    df_datos_biogeoquimicos   = df_datos_biogeoquimicos.replace(numpy.nan, None)
+    df_datos_fisicos          = df_datos_fisicos.replace(numpy.nan, None)
+
     id_dato_malo              = df_indices_calidad['indice'][df_indices_calidad['descripcion']=='Malo'].iloc[0]
     id_dato_bueno             = df_indices_calidad['indice'][df_indices_calidad['descripcion']=='Bueno'].iloc[0]
     id_dato_dudoso            = df_indices_calidad['indice'][df_indices_calidad['descripcion']=='Dudoso'].iloc[0]
