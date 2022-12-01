32ea456b95fe0b09bd58f8e8c1ac2c284989f258
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-12-01 15:10:38 +0100

    Update PRINCIPAL.py

--- PRINCIPAL.py
+++ PRINCIPAL.py
@@ -48,13 +48,13 @@ if FUNCIONES_AUXILIARES.log_in() is True:
                         }
 
             
-        # if st.session_state["usuario"] == 'COAC - Supervisión Nutrientes':
+        if st.session_state["usuario"] == 'COAC - Supervisión Nutrientes':
      
-        #     paginas = {"INICIO": PAGINAS.principal,
-        #             "ESTADO DEL PROCESADO DE MUESTRAS": PAGINAS.consulta_procesos, 
+            paginas = {"INICIO": PAGINAS.principal,
+                     "CONSULTA ESTADOS": PAGINAS.consulta_estado, 
         #             "INFORMACIÓN DISPONIBLE": PAGINAS.consulta_estado,
-        #             "GRÁFICO DE EVOLUCIÓN": PAGINAS.evolucion_analisis
-        #              }           
+                     "GRÁFICO DE EVOLUCIÓN": PAGINAS.evolucion_analisis
+                      }           
             
         # if st.session_state["usuario"] == 'Usuario externo':
             
