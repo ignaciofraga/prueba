9dfa903dec6deb99c8d70cdc7df5522784d5a5a0
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-12-14 15:59:04 +0100

    Update FUNCIONES_AUXILIARES.py

--- FUNCIONES/FUNCIONES_AUXILIARES.py
+++ FUNCIONES/FUNCIONES_AUXILIARES.py
@@ -547,7 +547,7 @@ def inserta_datos_biogeoquimicos(df_muestreos,df_datos_biogeoquimicos,variables_
           
             col1, col2,col3,col4 = st.columns(4,gap="small")
             with col1: 
-                if df_seleccion['botella'].iloc[idato]:
+                if df_seleccion['botella'].iloc[idato] is not None:
                     texto_botella = 'Botella:' + str(int(df_seleccion['botella'].iloc[idato]))
                     st.text(texto_botella)
                 
