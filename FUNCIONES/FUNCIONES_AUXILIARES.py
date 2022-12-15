24c97ca41ff614cd045760c5aef3236c047e57fe
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2022-12-15 08:55:34 +0100

    Update FUNCIONES_AUXILIARES.py

--- FUNCIONES/FUNCIONES_AUXILIARES.py
+++ FUNCIONES/FUNCIONES_AUXILIARES.py
@@ -526,7 +526,9 @@ def inserta_datos_biogeoquimicos(df_muestreos,df_datos_biogeoquimicos,variables_
         texto_error = "La base de datos ya contiene información para la salida, estación, cast y variable seleccionadas. Los datos introducidos reemplazarán los existentes."
         st.warning(texto_error, icon="⚠️") 
     else:
-        io_valores_prev = 0        
+        io_valores_prev = 0    
+        texto_error = "La base de datos NO contiene información para la salida, estación, cast y variable seleccionadas."
+        st.warning(texto_error, icon="⚠️") 
         
         
     df_seleccion    = df_seleccion.sort_values('botella')
