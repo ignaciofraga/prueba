2ae117de8bab9913898707b084e5a51821496ead
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-02-08 13:01:22 +0100

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -2033,19 +2033,12 @@ def procesado_nutrientes():
                 texto_exito = 'Muestreos disponibles procesados correctamente'
                 st.success(texto_exito)
                                
-                # Muestra una tabla con las configuraciones 
-                import st_aggrid 
-                gb = st_aggrid.grid_options_builder.GridOptionsBuilder.from_dataframe(datos_corregidos)
-                gridOptions = gb.build()
-                st_aggrid.AgGrid(datos_corregidos,gridOptions=gridOptions,enable_enterprise_modules=True,height = 350,fit_columns_on_grid_load = False,allow_unsafe_jscode=True,reload_data=True)    
-            
-                
-                
-                # Añade información de oxígeno, pH, alcalinidad, profunidad....a las muestras que ya estaban en la base de datos
+                # Añade información de oxígeno, pH, alcalinidad....de la base de datos (si está disponible)
                 df_datos_disponibles  = pandas.merge(df_datos_biogeoquimicos, df_datos_disponibles, on="muestreo")                               
                 df_datos_disponibles  = df_datos_disponibles.drop(columns=['ton','nitrato','nitrito','fosfato','silicato']) # Para evitar duplicidad de columnas
 
-                datos_corregidos      = pandas.merge(datos_corregidos, df_datos_disponibles, on="nombre_muestreo")  
+                if df_datos_disponibles.shape[0] == datos_corregidos.shape[0]:
+                    datos_corregidos      = pandas.merge(datos_corregidos, df_datos_disponibles, on="nombre_muestreo")  
                 
                 # Añade columna con información del año
                 datos_corregidos['año']                = numpy.zeros(datos_corregidos.shape[0],dtype=int)
