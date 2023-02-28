00bfd750eef14c49e43366a53ba0808e334eadb5
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-02-28 09:02:59 +0100

    Update FUNCIONES_LECTURA.py

--- FUNCIONES/FUNCIONES_LECTURA.py
+++ FUNCIONES/FUNCIONES_LECTURA.py
@@ -345,11 +345,11 @@ def lectura_archivo_perfiles(datos_archivo):
                 
             if texto_linea[0:22] == '* System UpLoad Time =': # Línea con hora del cast
                 listado_textos   = texto_linea.split('= ') 
-                datetime_sistema = datetime.datetime.strptime(listado_textos[-1],'%b %d %Y %H:%M:%S ')
+                datetime_sistema = datetime.datetime.strptime(listado_textos[-1],'%b %d %Y %H:%M:%S')
                                         
             if texto_linea[0:14] == '* System UTC =': # Línea con hora del cast
                 listado_textos    = texto_linea.split('= ')  
-                datetime_muestreo = datetime.datetime.strptime(listado_textos[-1],'%b %d %Y %H:%M:%S ')
+                datetime_muestreo = datetime.datetime.strptime(listado_textos[-1],'%b %d %Y %H:%M:%S')
                 
             if texto_linea[0:6] == '# name': # Línea con variable muestreada
                 posicion_inicio    = texto_linea.find('=') + 2
