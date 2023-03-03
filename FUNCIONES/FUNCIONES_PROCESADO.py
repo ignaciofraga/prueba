6439563d1164050805c607042359208b853e21e4
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-03-03 08:37:50 +0100

    Update FUNCIONES_PROCESADO.py

--- FUNCIONES/FUNCIONES_PROCESADO.py
+++ FUNCIONES/FUNCIONES_PROCESADO.py
@@ -871,11 +871,11 @@ def control_calidad_biogeoquimica(datos_procesados,variables_procesado,variables
         for ipunto in range(df_seleccion.shape[0]):     
             st.text(df_seleccion['presion_ctd'].iloc[ipunto])
             st.text(df_seleccion['botella'].iloc[ipunto])
-        #     if df_seleccion['botella'].iloc[ipunto] is None:
-        #         nombre_muestreos[ipunto] = 'Prof.' + str(int(df_seleccion['presion_ctd'].iloc[ipunto]))
-        #     else:
-        #         nombre_muestreos[ipunto] = 'Bot.' + str(int(df_seleccion['botella'].iloc[ipunto]))
-            
+            if df_seleccion['botella'].iloc[ipunto] is None:
+                nombre_muestreos[ipunto] = 'Prof.' + str(int(df_seleccion['presion_ctd'].iloc[ipunto]))
+            else:
+                nombre_muestreos[ipunto] = 'Bot.' + str(int(df_seleccion['botella'].iloc[ipunto]))
+            st.text(nombre_muestreos[ipunto])
         #     ax.annotate(nombre_muestreos[ipunto], (df_seleccion[variable_seleccionada].iloc[ipunto], df_seleccion['presion_ctd'].iloc[ipunto]))
                 
         # # Ajusta el rango de las x 
