d529c6fddfba3b31e6baa447c71f21ec4a4d76c7
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-03-27 09:26:59 +0200

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -154,7 +154,7 @@ def consulta_estado():
                     for iestado in range(len(nombre_estados)):    
                         if valor_estado == nombre_estados[iestado]:
                             color = "'" + colores_estados[iestado] + "'"
-                        return f'background-color: {color}'
+                            return f'background-color: {color}'
                     
                 # def color_survived(val):
                 #     color = 'green' if val else 'red'
