c2a4a9a6066027805f28e8118bb2f4a2a601641d
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-03-28 09:55:17 +0200

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -677,7 +677,7 @@ def entrada_salidas_mar():
     id_radiales                = df_programas['id_programa'][df_programas['nombre_programa']=='RADIAL CORUÑA'].tolist()[0]
     df_personal_comisionado    = df_personal[df_personal['comisionado']==True]
     df_personal_no_comisionado = df_personal[df_personal['comisionado']==False]
-    df_salidas_radiales        = df_salidas[df_salidas['nombre_programa']=='RADIAL CORUÑA']
+    df_salidas_radiales        = df_salidas[df_salidas['programa']==int(id_radiales)]
     df_estaciones_radiales     = df_estaciones[df_estaciones['programa']==int(id_radiales)]
   
     st.text(id_radiales)  
@@ -933,21 +933,16 @@ def entrada_salidas_mar():
                          
         
         # Despliega menús de selección del programa, tipo de salida, año y fecha               
-        col1, col2, col3= st.columns(3,gap="small")
+        col1, col2= st.columns(2,gap="small")
      
-        with col1: 
-            programa_seleccionado     = st.selectbox('Programa',(df_programas['nombre_programa']),index=id_radiales)   
-            df_salidas_seleccion      = df_salidas[df_salidas['nombre_programa']==programa_seleccionado]
-            
+        with col1:
+            tipo_salida_seleccionada  = st.selectbox('Tipo de salida',(df_salidas_radiales['tipo_salida'].unique()))   
+            df_salidas_seleccion      = df_salidas_radiales[df_salidas_radiales['tipo_salida']==tipo_salida_seleccionada]
         
-        with col2:
-            tipo_salida_seleccionada  = st.selectbox('Tipo de salida',(df_salidas_seleccion['tipo_salida'].unique()))   
-            df_salidas_seleccion      = df_salidas_seleccion[df_salidas_seleccion['tipo_salida']==tipo_salida_seleccionada]
-        
-            # Añade la variable año al dataframe
-            indices_dataframe               = numpy.arange(0,df_salidas_seleccion.shape[0],1,dtype=int)    
-            df_salidas_seleccion['id_temp'] = indices_dataframe
-            df_salidas_seleccion.set_index('id_temp',drop=False,append=False,inplace=True)
+            # # Añade la variable año al dataframe
+            # indices_dataframe               = numpy.arange(0,df_salidas_seleccion.shape[0],1,dtype=int)    
+            # df_salidas_seleccion['id_temp'] = indices_dataframe
+            # df_salidas_seleccion.set_index('id_temp',drop=False,append=False,inplace=True)
             
             # Define los años con salidas asociadas
             df_salidas_seleccion['año'] = numpy.zeros(df_salidas_seleccion.shape[0],dtype=int)
@@ -957,7 +952,7 @@ def entrada_salidas_mar():
             
             listado_anhos              = df_salidas_seleccion['año'].unique()
         
-        with col3:
+        with col2:
             anho_seleccionado           = st.selectbox('Año',(listado_anhos),index=len(listado_anhos)-1)
             df_salidas_seleccion        = df_salidas_seleccion[df_salidas_seleccion['año']==anho_seleccionado]
     
