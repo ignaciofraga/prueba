6bf8870448855cf7fec6a7b3005aa738f995a4f8
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-03-28 11:01:57 +0200

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -828,8 +828,8 @@ def entrada_salidas_mar():
             personal_no_comisionado_previo = datos_salida_seleccionada['participantes_no_comisionados'].iloc[0]
             estaciones_previas             = datos_salida_seleccionada['estaciones'].iloc[0] 
             id_buque_previo                = datos_salida_seleccionada['buque'].iloc[0] 
-            id_perfil_previo               = datos_salida_seleccionada['configuracion_perfilador'].iloc[0]  
-            id_sup_previo                  = datos_salida_seleccionada['configuracion_superficie'].iloc[0] 
+            id_perfil_previo               = datos_salida_seleccionada['configuracion_perfilador'].index(datos_salida_seleccionada['configuracion_perfilador'].iloc[0])
+            id_sup_previo                  = datos_salida_seleccionada['configuracion_superficie'].index(datos_salida_seleccionada['configuracion_superficie'].iloc[0])
             
             st.text(id_perfil_previo)
             st.text(id_sup_previo)
