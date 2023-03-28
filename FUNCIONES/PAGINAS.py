453806a66f3404e5800a98e92de7efbe3bb9caef
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-03-28 09:39:50 +0200

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -680,6 +680,8 @@ def entrada_salidas_mar():
     df_salidas_radiales        = df_salidas[df_salidas['nombre_programa']=='RADIAL CORUÑA']
     df_estaciones_radiales     = df_estaciones[df_estaciones['programa']==id_radiales]
   
+    st.dataframe(df_estaciones_radiales)  
+  
     # Fechas y horas por defecto
     fecha_actual        = datetime.date.today()
     hora_defecto_inicio = datetime.time(8,30,0,0,tzinfo = datetime.timezone.utc)
