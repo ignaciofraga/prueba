6be29ea3c036ab262a84281593053567147d37f6
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-04-24 10:22:44 +0200

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -1213,9 +1213,9 @@ def entrada_condiciones_ambientales():
                 if io_previo == 1:
                     texto_exito = 'Datos de las estación ' + estacion_elegida + ' durante la salida '  + salida  + ' actualizados correctamente'
                     
-                st.success(texto_exito)                
+            st.success(texto_exito)                
                 
-                st.cache_data.clear()
+            st.cache_data.clear()
                 
     # Descarga datos ambientales
     if tipo_entrada == entradas[1]:    
