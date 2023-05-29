b7504ebcab9ba2830a3882695fb57d9f5197de20
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-05-29 13:45:13 +0200

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -958,7 +958,9 @@ def entrada_salidas_mar():
                     instruccion_sql = '''INSERT INTO personal_salidas (id_personal,nombre_apellidos,comisionado)
                         VALUES (%s,%s,%s) ON CONFLICT (id_personal) DO UPDATE SET (nombre_apellidos,comisionado) = ROW(EXCLUDED.nombre_apellidos,EXCLUDED.correo,EXCLUDED.comisionado);''' 
                       
-                    st.text(id_asignado,nombre_participante,comision)  
+                    st.text(id_asignado)
+                    st.text(nombre_participante)
+                    st.text(comision)  
                       
                     conn = psycopg2.connect(host = direccion_host,database=base_datos, user=usuario, password=contrasena, port=puerto)
                     cursor = conn.cursor()
