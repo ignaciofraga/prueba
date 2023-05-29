3ed4be00ed1c6c8f353e01a912a39bc4ed53b6bc
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-05-29 13:48:01 +0200

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -965,7 +965,7 @@ def entrada_salidas_mar():
                     conn = psycopg2.connect(host = direccion_host,database=base_datos, user=usuario, password=contrasena, port=puerto)
                     cursor = conn.cursor()
                     if comision:
-                        cursor.execute(instruccion_sql, (id_asignado,nombre_participante,comision))
+                        cursor.execute(instruccion_sql, (id_asignado,nombre_participante,True))
                     else:
                         cursor.execute(instruccion_sql, (id_asignado,nombre_participante,False))                        
                     conn.commit()
