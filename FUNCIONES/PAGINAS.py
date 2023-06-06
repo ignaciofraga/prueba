32fd7ad59decb78d7bee0a5b74399c6abc096fd4
ignaciofraga <110227934+ignaciofraga@users.noreply.github.com>
2023-06-06 10:23:09 +0200

    ss

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -1607,19 +1607,21 @@ def entrada_archivos_roseta():
         # procesa ese dataframe
         io_control_calidad = 1
         indice_programa,indice_estacion,indice_salida,cast_seleccionado,meses_offset,variable_seleccionada,salida_seleccionada = FUNCIONES_AUXILIARES.menu_seleccion(df_datos_disponibles,variables_procesado,variables_procesado_bd,io_control_calidad,df_salidas,df_estaciones,df_programas)
-                                                   
+                                       
+        st.text(indice_programa)
+        st.text(indice_estacion)
+        st.text(indice_salida)
+        st.text(cast_seleccionado)
+        st.text(salida_seleccionada)
+        st.text(variable_seleccionada)
+            
         # Recupera el nombre "completo" de la variable y sus unidades
         indice_variable          = variables_procesado_bd.index(variable_seleccionada)
         nombre_completo_variable = variables_procesado[indice_variable] 
         unidades_variable        = variables_unidades[indice_variable]
         tabla_insercion          = variable_tabla[indice_variable]
                         
-                                                
-        st.text(indice_variable)
-        st.text(nombre_completo_variable)
-        st.text(unidades_variable)
-        st.text(tabla_insercion)
-        
+                                                        
         # Selecciona los datos correspondientes al programa, estación, salida y cast seleccionados
         datos_procesados     = df_datos_disponibles[(df_datos_disponibles["programa"] == indice_programa) & (df_datos_disponibles["estacion"] == indice_estacion) & (df_datos_disponibles["salida_mar"] == indice_salida) & (df_datos_disponibles["num_cast"] == cast_seleccionado)]
 
