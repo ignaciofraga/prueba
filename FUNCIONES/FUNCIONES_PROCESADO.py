89494b5cb0413fd3cd64bd31f6760e82707828b8
ignaciofraga <110227934+ignaciofraga@users.noreply.github.com>
2023-06-29 13:00:29 +0200

    Update FUNCIONES_PROCESADO.py

--- FUNCIONES/FUNCIONES_PROCESADO.py
+++ FUNCIONES/FUNCIONES_PROCESADO.py
@@ -912,16 +912,17 @@ def control_calidad_biogeoquimica(datos_procesados,datos_disponibles_bd,variable
         if not datos_procesados['oxigeno_ctd'].isnull().all(): 
 #            datos_procesados.oxigeno_ctd.fillna(value=numpy.nan, inplace=True)
 #            
-#            df_oxigeno = datos_procesados.loc[datos_procesados['oxigeno_ctd'].notnull(), ['oxigeno_ctd','presion_ctd']]
-#            az.plot(df_oxigeno['oxigeno_ctd'],df_oxigeno['presion_ctd'],'.',color='#006633',label='OXIMETRO')
+            df_oxigeno = datos_procesados.loc[datos_procesados['oxigeno_ctd'].notnull(), ['oxigeno_ctd','presion_ctd']]
+            st.dataframe(df_oxigeno)
+            az.plot(df_oxigeno['oxigeno_ctd'],df_oxigeno['presion_ctd'],'.',color='#006633',label='OXIMETRO')
             az.plot(datos_procesados['oxigeno_ctd'],datos_procesados['presion_ctd'],'.',color='#006633',label='OXIMETRO')
             io_plot = 1
-            st.text(1)
+
                 
         if not datos_procesados['oxigeno_wk'].isnull().all(): 
             az.plot(datos_procesados['oxigeno_wk'],datos_procesados['presion_ctd'],'.',color='#00CC66',label='WINKLER')
             io_plot = 1
-            st.text(2)
+
             
         st.dataframe(datos_procesados)
             
