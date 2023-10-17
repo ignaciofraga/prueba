aa60e87c7fec4608ce531ee5cb94f819a9d5e298
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-10-17 12:12:13 +0200

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -1745,7 +1745,7 @@ def procesado_nutrientes():
                 # datos_corregidos = pandas.merge(datos_corregidos, df_datos_biogeoquimicos, on="muestreo",how='left')
                 
                 
-                
+                st.dataframe(datos_corregidos)
                 
                 variables_elimina       = variables_procesado_bd + ['rto_columna_procesado','temp_lab_procesado','rmn_bajo_procesado','rmn_alto_procesado']
                 df_datos_biogeoquimicos = df_datos_disponibles.drop(columns=variables_elimina)
