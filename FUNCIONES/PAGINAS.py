ac55b927a523b752801dd466526d595879bd87a6
ignaciofraga <110227934+ignaciofraga@users.noreply.github.com>
2023-11-15 08:46:08 +0100

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -1691,10 +1691,6 @@ def procesado_nutrientes():
             datos_referencias = datos_AA[datos_AA['Sample ID'].str.contains(rmn_elegida_bajo)]
             ref_final         = datos_referencias['Peak Number'].iloc[1] - 2
             
-            st.text(ref_inicial)
-            st.text(ref_final)
-
-            
             # Encuentra la salinidad de cada muestra
             datos_AA['salinidad']     = numpy.ones(datos_AA.shape[0])
             datos_AA['io_procesado']  = None
