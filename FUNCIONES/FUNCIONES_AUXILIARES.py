848902e65602b0735a199a76846c4dcff929604f
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-12-05 09:18:45 +0100

    Update FUNCIONES_AUXILIARES.py

--- FUNCIONES/FUNCIONES_AUXILIARES.py
+++ FUNCIONES/FUNCIONES_AUXILIARES.py
@@ -914,7 +914,7 @@ def consulta_botellas():
                             for ivariable_listadas in range(len(listado_variables_listadas)):
                                 listado_temp    = datos_prof[listado_variables_listadas[ivariable_listadas]]
                                 listado_no_nulo = [item for item in listado_temp if item is not None]
-                                st.text(len(listado_variables_listadas))
+                                st.text(listado_variables_listadas[ivariable_listadas])
                                 st.text(listado_no_nulo)
                                 if len(listado_no_nulo)>0:
                                     try:
