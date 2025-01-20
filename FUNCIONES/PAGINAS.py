a394df866fe5847b7c428ab6577c750c7a78de4c
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2025-01-20 13:55:38 +0100

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -1425,7 +1425,7 @@ def procesado_nutrientes():
                 subset_inicio = datos_corregidos.iloc[registros_inicio, :]
                 subset_final = datos_corregidos.iloc[registros_final, :]
                 
-
+                st.dataframe(subset_final)
 
                 
                 datos_corregidos  = pandas.merge(datos_corregidos, df_estaciones, on="estacion")
