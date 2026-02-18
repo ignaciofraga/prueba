20e1a81f68c593f4e818b64214b3b513481bd2ad
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2026-02-18 14:23:46 +0100

    Update FUNCIONES_PROCESADO.py

--- FUNCIONES/FUNCIONES_PROCESADO.py
+++ FUNCIONES/FUNCIONES_PROCESADO.py
@@ -1673,7 +1673,8 @@ def correccion_drift(datos_entrada,df_referencias_altas,df_referencias_bajas,var
         pte_RMN      = (RMN_ALTO_variable-RMN_BAJO_variable)/(RMN_altos.iloc[0]-RMN_bajos.iloc[0]) 
         t_indep_RMN  = RMN_BAJO_variable- pte_RMN*RMN_bajos.iloc[0] 
     
-        st.text(variable_concentracion,str(pte_RMN))
+        st.text(variable_concentracion)
+        st.text(str(pte_RMN))
     
         variable_drift = numpy.zeros(datos_entrada.shape[0])
     
