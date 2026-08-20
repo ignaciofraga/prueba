6247c4a7cc086dc0b78a7570fff6386ec4a149d5
ignaciofraga <110227934+ignaciofraga@users.noreply.github.com>
2026-08-20 12:53:15 +0200

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -485,26 +485,13 @@ def entrada_salidas_mar():
             df_salidas_seleccion['Fecha retorno'].iloc[idato]  =  df_salidas_seleccion['Fecha retorno'].iloc[idato].strftime("%Y-%m-%d")
 
 
-
-
-        #st.dataframe(df_salidas_seleccion)
-
         # Ordena los valores por fechas
-        #df_salidas_seleccion = df_salidas_seleccion.sort_values('Fecha salida',ascending=False)
-        df_salidas_seleccion = df_salidas_seleccion.sort_values(by='Fecha salida')
+        df_salidas_seleccion = df_salidas_seleccion.sort_values(by='Fecha salida', ascending=False)
         
-        #df_salidas_seleccion['Fecha salida'] =  pandas.to_datetime(df_salidas_seleccion['Fecha salida']).dt.strftime('%Y-%m-%d')
-        #df_salidas_seleccion = df_salidas_seleccion.sort_values(by=['Fecha salida'],axis=0, ascending=False)
-
-
-
-
         # Mueve los identificadores de muestreo al final del dataframe
         listado_cols = df_salidas_seleccion.columns.tolist()
         listado_cols.append(listado_cols.pop(listado_cols.index('Observaciones')))   
         df_salidas_seleccion = df_salidas_seleccion[listado_cols]
-        
-        
         
         # Define un nuevo índice de filas. Si se han eliminado registros este paso es necesario
         indices_dataframe        = numpy.arange(0,df_salidas_seleccion.shape[0],1,dtype=int)    
