75584b33196782a9c893d5b9f88b8ee6f8c0b958
ignaciofraga <110227934+ignaciofraga@users.noreply.github.com>
2026-09-03 09:19:01 +0200

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -1756,13 +1756,17 @@ def procesado_nutrientes():
                 # Actualiza el estado de los procesos
                 fecha_actualizacion = datetime.date.today()
                 
-                instruccion_sql = 'UPDATE planificacion_analisis_nutrientes SET fecha_analisis =%s, io_analizado=%s, temperatura_laboratorio=%s, rendimiento_columna=%s,id_rmn_bajo=%s, id_rmn_alto  WHERE lote = %s AND nombre = %s;'
+                #instruccion_sql = 'UPDATE planificacion_analisis_nutrientes SET fecha_analisis =%s, io_analizado=%s, temperatura_laboratorio=%s, rendimiento_columna=%s,id_rmn_bajo=%s, id_rmn_alto  WHERE lote = %s AND nombre = %s;'
 
                 conn = psycopg2.connect(host = direccion_host,database=base_datos, user=usuario, password=contrasena, port=puerto)
                 
 
                 cursor = conn.cursor()
-                cursor.execute(instruccion_sql, (fecha_actualizacion,True,temperatura_laboratorio,rendimiento_columna,int(id_ref_bajo),int(id_ref_alto),lote_analisis,int(id_solicitud_analisis)))
+                #cursor.execute(instruccion_sql, (fecha_actualizacion,True,temperatura_laboratorio,rendimiento_columna,int(id_ref_bajo),int(id_ref_alto),lote_analisis,int(id_solicitud_analisis)))
+                #instruccion_sql = 'UPDATE planificacion_analisis_nutrientes SET fecha_analisis =%s, io_analizado=%s, temperatura_laboratorio=%s, rendimiento_columna=%s,id_rmn_bajo=%s, id_rmn_alto  WHERE lote = %s AND nombre = %s;'
+                instruccion_sql = 'UPDATE planificacion_analisis_nutrientes SET fecha_analisis =%s  WHERE lote = %s AND nombre = %s;'
+                cursor.execute(instruccion_sql, (fecha_actualizacion,lote_analisis,int(id_solicitud_analisis)))
+               
                 conn.commit()
                 cursor.close()
                     
@@ -2009,12 +2013,12 @@ def procesado_nutrientes():
 
 
 
-    # Añade manualmente resultados del procesado 
-    if tipo_accion == acciones[1]:
+    # # Añade manualmente resultados del procesado 
+    # if tipo_accion == acciones[1]:
         
-        st.subheader('Inserción de datos de nutrientes')
+    #     st.subheader('Inserción de datos de nutrientes')
         
-        FUNCIONES_AUXILIARES.inserta_datos_biogeoquimicos(df_muestreos,df_datos_biogeoquimicos,variables_procesado,variables_procesado_bd,df_referencia)
+    #     FUNCIONES_AUXILIARES.inserta_datos_biogeoquimicos(df_muestreos,df_datos_biogeoquimicos,variables_procesado,variables_procesado_bd,df_referencia)
 
         
 
