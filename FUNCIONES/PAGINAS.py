74886605929d71ae503880f9726deb95d16b8af8
ignaciofraga <110227934+ignaciofraga@users.noreply.github.com>
2026-09-07 12:59:56 +0200

    Update PAGINAS.py

--- FUNCIONES/PAGINAS.py
+++ FUNCIONES/PAGINAS.py
@@ -2795,7 +2795,11 @@ def planificacion_procesos():
                     
             conn = psycopg2.connect(host = direccion_host,database=base_datos, user=usuario, password=contrasena, port=puerto)
             
-            
+            df_modificado = df_modificado.reset_index()
+
+            # drop it
+            df_modificado = df_modificado.reset_index(drop=True)
+
             df_modificado.index.to_frame()# df_modificado["lote_temp"] = df_modificado["lote"]
             
             # indices_dataframe              = numpy.arange(0,df_modificado.shape[0],1,dtype=int)    
